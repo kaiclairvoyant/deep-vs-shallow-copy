477e47c84e9b689fb4128d49192e33d9856640e0
kaiclairvoyant <rodrigosantorato@hotmail.com>
2022-07-01 00:49:20 -0300

    moving project to src and adding \n for readability

--- src/structs.go
+++ src/structs.go
@@ -13,24 +13,24 @@ func copyStructs() {
 	p.p = newPointerTest("second pointer")
 	c := Character{n, l, p}
 
-	fmt.Println("original object:\n" + render.Render(c))
+	fmt.Println("original object:\n\n" + render.Render(c) + "\n")
 
 	var deepCopyCandidate Character
 	deepCopyCandidate = c
 	deepCopyCandidate.power.name = "Shallow Coding Skills"
 	deepCopyCandidate.level.nested.number = 2
 	deepCopyCandidate.p1.p.name = "shallow pointer"
-	fmt.Println("original object after shallow copy:\n" + render.Render(c))
-	fmt.Println("shallow copy:\n" + render.Render(deepCopyCandidate))
+	fmt.Println("original object after shallow copy:\n\n" + render.Render(c) + "\n")
+	fmt.Println("shallow copy:\n\n" + render.Render(deepCopyCandidate) + "\n")
 
 	var shallowCopy *Character
 	shallowCopy = &c
 	shallowCopy.power.name = "Mad Coding Skills"
 	shallowCopy.level.nested.number = 3
 	shallowCopy.p1.name = "deep pointer"
-	fmt.Println("original object after deep copy:\n" + render.Render(c))
-	fmt.Println("deep copy:\n" + render.Render(shallowCopy))
-	fmt.Println("conclusion:\nit can be deceitful to assign variables when trying to deep copy, as the objects " +
-		"can be changed without affecting the original memory addresses, but when copying a pointer it will still " +
-		"continue to point to the same memory address")
+	fmt.Println("original object after deep copy:\n\n" + render.Render(c) + "\n")
+	fmt.Println("deep copy:\n\n" + render.Render(shallowCopy) + "\n")
+	fmt.Println("conclusion:\n\nit can be deceitful to assign variables when trying to deep copy, as most " +
+		"objects can be changed without affecting the original memory addresses, but when copying a pointer it will " +
+		"still keep pointing to the same memory address")
 }
