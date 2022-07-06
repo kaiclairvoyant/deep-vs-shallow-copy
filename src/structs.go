3277416a219eac8079a90c21264f09d0f503045b
kaiclairvoyant <rodrigosantorato@hotmail.com>
2022-07-05 23:17:51 -0300

    removing unnecessary code from structs.go and renaming variables to increase readability

--- src/structs.go
+++ src/structs.go
@@ -6,6 +6,7 @@ import (
 )
 
 func copyStructs() {
+	fmt.Println("____________Initiating struct copies____________")
 	nested := NestedNumber{1}
 	l := Level{nested}
 	n := Power{"Super Intelligence"}
@@ -13,24 +14,17 @@ func copyStructs() {
 	p.p = newPointerTest("second pointer")
 	c := Character{n, l, p}
 
-	fmt.Println("original object:\n\n" + render.Render(c) + "\n")
+	fmt.Println("original object:\n" + render.Render(c) + "\n")
 
-	var fakeDeep Character
-	fakeDeep = c
-	fakeDeep.power.name = "Fake Deep Coding Skills"
-	fakeDeep.level.nested.number = 2
-	fakeDeep.p1.p.name = "fake deep pointer"
-	fmt.Println("original object after fake deep copy:\n\n" + render.Render(c) + "\n")
-	fmt.Println("fake deep copy:\n\n" + render.Render(fakeDeep) + "\n")
+	var testCopy Character
+	testCopy = c
+	testCopy.power.name = "Shallow Copy"
+	testCopy.level.nested.number = 2
+	testCopy.p1.p.name = "shallow pointer"
+	fmt.Println("original object after shallow copy:\n" + render.Render(c) + "\n")
+	fmt.Println("shallow copy:\n" + render.Render(testCopy) + "\n")
 
-	var shallowCopy *Character
-	shallowCopy = &c
-	shallowCopy.power.name = "Mad Coding Skills"
-	shallowCopy.level.nested.number = 3
-	shallowCopy.p1.name = "shallow pointer"
-	fmt.Println("original object after shallow copy:\n\n" + render.Render(c) + "\n")
-	fmt.Println("shallow copy:\n\n" + render.Render(shallowCopy) + "\n")
-	fmt.Println("conclusion:\n\nit can be deceitful to assign variables when trying to shallow copy, as most " +
-		"objects can be changed without affecting the original memory addresses, but when copying a pointer it will " +
-		"still keep pointing to the same memory address")
+	fmt.Println("conclusion:\nit can be deceitful to assign variables when trying to create a deep copy, as most " +
+		"structs can be changed without affecting the original memory addresses, but when copying a pointer it will " +
+		"still keep pointing to the same memory address of the original pointer")
 }
